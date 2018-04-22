b02635b2a6c26b60627eb13499e2721fc9909a84
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-04-21 17:41:21 -0700

    .old/235/lib/yi_0sys.py -> fecon236/util/system.py
    Moved and renamed module, with cosmetic flake8 edits.
    
    Need to fix:
    system.py:42:1: F401 'subprocess.STDOUT' imported but unused
    system.py:134:20: F821 undefined name 'vermod'
    system.py:135:9: E722 do not use bare except'
    system.py:145:5: E722 do not use bare except'
    system.py:147:14: E711 comparison to None should be 'if cond is None:'
    system.py:199:12: F821 undefined name 'CalledProcessError'

--- fecon236/util/system.py
+++ fecon236/util/system.py
@@ -1,7 +1,7 @@
 #  Python Module for import                           Date : 2017-05-15
-#  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per Python PEP 0263 
-''' 
-_______________|  yi_0sys.py : system and date functions including specs.
+#  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
+'''
+_______________|  system.py :: system and date functions including specs.
 
 Code in this module must be compatible with both Python 2 and 3.
 It is a bridge and a guardian between the two Pythons.
@@ -19,17 +19,17 @@ REFERENCES:
 
 CHANGE LOG  For latest version, see https://github.com/rsvp/fecon235
 2017-05-15  Add timestamp() per strict RFC-3339 standard.
-               Also include scipy and sympy in specs().
+                Also include scipy and sympy in specs().
 2017-03-16  Revise: minimumPandas = 18.0
 2016-04-28  Use version('jupyter_core') instead of version('jupyter').
-               Update to PREAMBLE-p6.16.0428
+                Update to PREAMBLE-p6.16.0428
 2015-12-29  For errf in gitinfo(), our dev_null instead of os.devnull
-               Add minimumPandas variable esp. for tests.
+                Add minimumPandas variable esp. for tests.
 2015-12-27  Get jupyter version among specs().
-               Fix run command to accept errf argument.
-               For specs(), move gitinfo try clause therein.
+                Fix run command to accept errf argument.
+                For specs(), move gitinfo try clause therein.
 2015-12-23  Add run command and gitinfo functions.
-               Update to PREAMBLE-p6.15.1223
+                Update to PREAMBLE-p6.15.1223
 2015-12-19  python3 compatible: absolute_import
 2015-12-03  First version.
 '''
@@ -43,7 +43,7 @@ from subprocess import check_output, STDOUT
 #                      ^for Python 2.7 and 3+
 
 
-minimumPython = ( 2, 7, 0 )
+minimumPython = (2, 7, 0)
 #             ... else a warning is generated in specs().
 minimumPandas = 18.0
 #               ^after replacing the first dot, then float.
@@ -51,7 +51,7 @@ minimumPandas = 18.0
 
 #             Open  /dev/null equivalent file for Unix and Windows:
 dev_null = os.open(os.devnull, os.O_RDWR)
-#                                 ^Cross-platform read and write. 
+#                                 ^Cross-platform read and write.
 #             os.devnull is just a string:  "/dev/null" or "nul"
 #             thus redirecting to os.devnull is insufficient
 #             and that alone will cause a fileno error.
@@ -72,23 +72,23 @@ def program():
     return os.path.basename(sys.argv[0])
 
 
-def warn( message, stub="WARNING:", prefix=" !. "):
+def warn(message, stub="WARNING:", prefix=" !. "):
     '''Write warning solely to standard error.'''
     print(prefix, stub, program(), message, sep=' ', file=sys.stderr)
 
 
-def die( message, errcode=1, prefix=" !! "):
+def die(message, errcode=1, prefix=" !! "):
     '''Gracefully KILL script, optionally specifying error code.'''
     stub = "FATAL " + str(errcode) + ":"
-    warn( message, stub, prefix )
-    sys.exit( errcode )
+    warn(message, stub, prefix)
+    sys.exit(errcode)
     #         ^interpretation is system dependent;
     #          generally non-zero is considered as some error.
-    #  Note: "os._exit" exits without calling cleanup handlers, 
+    #  Note: "os._exit" exits without calling cleanup handlers,
     #  flushing stdio buffers, etc. Thus, it is not a standard way.
 
 
-def date( hour=True, utc=True, localstr=' Local' ):
+def date(hour=True, utc=True, localstr=' Local'):
     '''Get date, and optionally time, as ISO string representation.
        Boolean hour variable also gives minutes and seconds.
        Setting utc to False will give local time instead of UTC,
@@ -104,14 +104,14 @@ def date( hour=True, utc=True, localstr=' Local' ):
     else:
         form += localstr
         tup = time.localtime()
-    return time.strftime( form, tup ) 
+    return time.strftime(form, tup)
 
 
 def timestamp():
     '''Timestamp per strict RFC-3339 standard where timezone Z:=UTC.'''
     form = "%Y-%m-%dT%H:%M:%SZ"
     tup = time.gmtime()
-    return time.strftime( form, tup ) 
+    return time.strftime(form, tup)
 
 
 def pythontup():
@@ -121,41 +121,41 @@ def pythontup():
     return sys.version_info[:3]
 
 
-def versionstr( module="IPython" ):
+def versionstr(module="IPython"):
     '''Represent version as a string, or None if not installed.'''
     #  Unfortunately must treat Python vs its modules differently...
-    if module=="Python" or module=="python":
+    if module == "Python" or module == "python":
         ver = pythontup()
-        return str(ver[0]) + '.' + str(ver[1]) + '.' + str(ver[2])   
-    else: 
+        return str(ver[0]) + '.' + str(ver[1]) + '.' + str(ver[2])
+    else:
         try:
-            exec( "import " + module )
-            exec( "vermod = " + module + ".__version__" )
+            exec("import " + module)
+            exec("vermod = " + module + ".__version__")
             return vermod
         except:
             return None
 
 
-def versiontup( module="IPython" ):
+def versiontup(module="IPython"):
     '''Parse version string into some integer 3-tuple.'''
     s = versionstr(module)
     try:
-        v = [ int(k) for k in s.split('.') ]
+        v = [int(k) for k in s.split('.')]
         return tuple(v)
     except:
         #  e.g. if not installed or not convertible to integers...
         if s == None:
-            return ( 0,  0,  0)
+            return (0,  0,  0)
         else:
             return (-9, -9, -9)
 
 
-def version( module="IPython" ):
+def version(module="IPython"):
     '''Pretty print Python or module version info.'''
     print(" :: ", module, versionstr(module))
 
 
-def utf( immigrant, xnl=True ):
+def utf(immigrant, xnl=True):
     '''Convert to utf-8, and possibly delete new line character.
        xnl means "delete new line"
     '''
@@ -167,21 +167,20 @@ def utf( immigrant, xnl=True ):
         return immigrant.decode('utf-8')
 
 
-def run( command, xnl=True, errf=None ):
+def run(command, xnl=True, errf=None):
     '''RUN **quote and space insensitive** SYSTEM-LEVEL command.
-       OTHERWISE: use check_output directly and list component 
-       parts of the command, e.g.  
-          check_output(["git", "describe", "--abbrev=0"])
-       then generally use our utf() since check_output 
-       usually does not return utf-8, so be prepared to 
+       OTHERWISE: use check_output directly and list component
+       parts of the command, e.g.
+        check_output(["git", "describe", "--abbrev=0"])
+       then generally use our utf() since check_output
+       usually does not return utf-8, so be prepared to
        receive bytes and also new line.
     '''
     #  N.B. -  errf=None means the usual error transmittal.
     #          Cross-platform /dev/stdout is STDOUT
     #          Cross-platform /dev/null   is our dev_null above.
     #  https://docs.python.org/2/library/subprocess.html
-    return utf( check_output(command.split(), stderr=errf), xnl )
-
+    return utf(check_output(command.split(), stderr=errf), xnl)
 
 
 def gitinfo():
@@ -219,10 +218,10 @@ def specs():
     version("sympy")
     version("pandas")
     version("pandas_datareader")
-    #       ^but package is "pandas-datareader" esp. for financial quotes. 
+    #       ^but package is "pandas-datareader" esp. for financial quotes.
     repo, tag, bra = gitinfo()
-    print(" ::  Repository:", repo, tag, bra )
-    print(" ::  Timestamp:", timestamp() )
+    print(" ::  Repository:", repo, tag, bra)
+    print(" ::  Timestamp:", timestamp())
 
 
 if pythontup() < (3, 0, 0):
@@ -243,7 +242,6 @@ def endmodule():
 
 if __name__ == "__main__":
     endmodule()
-
 
 
 '''
@@ -270,22 +268,23 @@ print(" ::  $pwd:", pwd)
 #  Notebook DISPLAY options:
 #      Represent pandas DataFrames as text; not HTML representation:
 import pandas as pd
-pd.set_option( 'display.notebook_repr_html', False )
+pd.set_option('display.notebook_repr_html', False)
 from IPython.display import HTML # useful for snippets
-#  e.g. HTML('<iframe src=http://en.mobile.wikipedia.org/?useformat=mobile width=700 height=350></iframe>')
-from IPython.display import Image 
-#  e.g. Image(filename='holt-winters-equations.png', embed=True) # url= also works
+#  e.g. HTML('<iframe src=http://en.mobile.wikipedia.org/?useformat=mobile
+#            width=700 height=350></iframe>')
+from IPython.display import Image
+#  e.g. Image(filename='holt-winters-equations.png', embed=True)
+#                  url= Also works instead of filename.
 from IPython.display import YouTubeVideo
 #  e.g. YouTubeVideo('1j_HxD4iLn8', start='43', width=600, height=400)
 from IPython.core import page
 get_ipython().set_hook('show_in_pager', page.as_hook(page.display_page), 0)
-#  Or equivalently in config file: "InteractiveShell.display_page = True", 
+#  Or equivalently in config file: "InteractiveShell.display_page = True",
 #  which will display results in secondary notebook pager frame in a cell.
 
 #  Generate PLOTS inside notebook, "inline" generates static png:
-%matplotlib inline   
+%matplotlib inline
 #          "notebook" argument allows interactive zoom and resize.
 
 
 '''
-
