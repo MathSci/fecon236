d9a7bcfeadca80de347c71547d9cd3cc3f0f8fb9
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-04-25 09:10:04 -0700

    Add fecon236/core.py, a dummy for now

---
+++ fecon236/core.py
@@ -0,0 +1,22 @@
+#  Python Module for import                           Date : 2018-04-23
+#  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
+'''
+_______________|  fecon236/core.py :: To be executed by __init__.py
+
+     References:  http://docs.python-guide.org/en/latest/writing/structure
+                  https://github.com/kennethreitz/samplemod
+
+CHANGE LOG  For latest version, see https://git.io/fecon236
+2018-04-23  First version.
+'''
+
+# py2rm
+from __future__ import absolute_import, print_function
+
+
+def hellocore():
+    print("Hello from core.py!")
+
+
+if __name__ == "__main__":
+    hellocore()
