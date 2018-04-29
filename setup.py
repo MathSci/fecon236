5751788e5b1059c21ef7aca1e89bb7dbe1b37f4b
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-04-29 15:09:00 -0700

    setup.py: More directives and references
    Compare to requirements.txt functionalities.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 #  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
-#  Python package installation                        Date : 2018-04-23
+#  Python package installation                        Date : 2018-04-30
 '''
 _______________|  fecon236/setup.py :: Installation via setuptools.
 
                   Essential install config file, esp. for pip.
 
-           Rant:  pip does NOT properly resolve dependencies.
+           Rant:  pip CANNOT properly resolve dependencies!
                   Setting up installation is frustrating in the
                   Python ecosystem with complex and magical incantations.
 
@@ -15,11 +15,24 @@ _______________|  fecon236/setup.py :: Installation via setuptools.
 
      References:  Donald Stufft, 2013, "setup.py vs requirements.txt"
                   https://caremad.io/posts/2013/07/setup-vs-requirement
+     https://packaging.python.org/tutorials/distributing-packages       <=!!
      https://packaging.python.org/guides/single-sourcing-package-version
+     https://packaging.python.org/guides -- Idiosyncrasies
+     https://packaging.python.org/discussions/wheel-vs-egg -- No eggs!
+     https://packaging.python.org/specifications -- PyPA SPECS.
+
+       For PyPI:  - Distinguish between "Universal" and "Pure" wheels.
+                  - Create your distribution (wheels):
+                        pythonN setup.py bdist_wheel [--universal]
+                  - dist/ will be under your project’s root directory.
+                  - To upload to PyPI:
+                        twine upload dist/*
+                  - Check https://pypi.org/project/<PROJECT>
 
      CLASSIFERS:  https://pypi.python.org/pypi?%3Aaction=list_classifiers
 
 CHANGE LOG  For latest version, see https://git.io/fecon236
+2018-04-30  Additional directives, requirements.txt comparison.
 2018-04-23  First version.
 '''
 
@@ -30,11 +43,38 @@ from setuptools import setup, find_packages
 PROJECT = 'fecon236'
 
 #  What packages are required? For example:
-#  'requests', 'maya', 'records',
+#  'foo', 'bar>=2.0', 'foobar~=3.4.5'
 #  ___ATTN___ But does this list start their installations??
 #             How is this connected to requirements.txt??
 #             What if prequisites are already furnished by Anaconda?
+#  https://packaging.python.org/discussions/install-requires-vs-requirements
+#  "Specify what a project MINIMALLY needs to run correctly.
+#  When the project is installed by pip,
+#  this is the specification that is used to install its dependencies.
+#  NOT considered best practice to use install_requires to pin
+#  dependencies to specific versions, or to specify sub-dependencies.
+#  Note: install_requires is a listing of ABSTRACT requirements,
+#  i.e just names and version restrictions that do not determine
+#  where the dependencies will be fulfilled. The concreteness
+#  will be determined at install time using pip options."
+#          "Whereas "install_requires" defines the dependencies
+#          for a single project, requirements.txt is used to define
+#          requirements for a complete Python ENVIRONMENT.
+#          Requirements files often contain an exhaustive listing
+#          of pinned versions for the purpose of achieving
+#          repeatable installations of a complete environment."
+#  However, using "pip install -e . " which is short for --editable,
+#  and "." refering to the current working directory, will install
+#  the current directory (i.e. your project) in editable mode.
+#  This will also INSTALL any dependencies declared with "install_requires"
+#  and any scripts declared with "console_scripts".
+#  Dependencies will be installed in the usual, non-editable mode.
+#      If you don’t want to install any dependencies at all:
+#              pip install -e . --no-deps
+
 REQUIRED = []
+#          ^Leave EMPTY instead of relying on --no-deps
+#           (We do not want conflicts with conda installations.)
 
 
 with open('README.md') as f:
@@ -59,10 +99,16 @@ setup(
     author_email='MathSci-github@googlegroups.com',
     python_requires='>=2.7.0',
     url='https://github.com/MathSci/'+PROJECT,
+    project_urls={
+        'Source': 'https://github.com/MathSci/'+PROJECT,
+        'Documentation': 'https://github.com/MathSci/'+PROJECT+'/docs',
+        'Tracker': 'https://github.com/MathSci/'+PROJECT+'/issues',
+    },
     install_requires=REQUIRED,
     include_package_data=True,
     packages=find_packages(exclude=('.old', 'docs', 'tests')),
     license=licensed,
+    keywords='pandas finance economics statistics econometrics jupyter',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
@@ -75,7 +121,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython'
     ],
 )
