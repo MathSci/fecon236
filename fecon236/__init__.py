f290f4ac5b47ec8140754a4aed6adc8778b1c629
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-05-22 15:34:55 -0700

    fecon236/__init__.py: Import module host/qdl.py

--- fecon236/__init__.py
+++ fecon236/__init__.py
@@ -1,5 +1,5 @@
 #  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
-#  Python package installation                        Date : 2018-05-15
+#  Python package installation                        Date : 2018-05-22
 '''
 _______________|  fecon236/__init__.py :: Top installation goods.
 
@@ -14,6 +14,7 @@ References:  https://github.com/kennethreitz/samplemod
              https://github.com/kennethreitz/setup.py
 
 CHANGE LOG  For latest version, see https://git.io/fecon236
+2018-05-22  Include qdl module.
 2018-05-15  Include plot() as principal.
 2018-05-13  Include principal modules.
 2018-05-01  Clarify use of hand-coded __version__.
@@ -30,12 +31,13 @@ from fecon236.core import hellocore                       # noqa
 from fecon236.tool import *                               # noqa
 from fecon236.util import system                          # noqa
 from fecon236.host import fred                            # noqa
+from fecon236.host import qdl                             # noqa
 from fecon236.visual import plots                         # noqa
 from fecon236.visual.plots import plot                    # noqa
 from fecon236.tsa import holtwinters as hw                # noqa
 
 
-__version__ = '10.6.1a24.post180513'
+__version__ = '10.6.2a29.post180522'
 #             ^HAND-CODE this string on MAJOR.MINOR releases.
 #              The alpha tag is based on Travis build number.
 #              The .post handle here is date-based.
