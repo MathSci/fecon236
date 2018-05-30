fcecce52b1780e1e013c80795daacdea6f563dfe
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-05-30 09:10:01 -0700

    fecon236/__init__.py: Include optimize module
    Deleted: fecon236/core.py, and its experimental inclusion.

--- fecon236/__init__.py
+++ fecon236/__init__.py
@@ -1,5 +1,5 @@
 #  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
-#  Python package installation                        Date : 2018-05-22
+#  Python package installation                        Date : 2018-05-30
 '''
 _______________|  fecon236/__init__.py :: Top installation goods.
 
@@ -14,6 +14,7 @@ References:  https://github.com/kennethreitz/samplemod
              https://github.com/kennethreitz/setup.py
 
 CHANGE LOG  For latest version, see https://git.io/fecon236
+2018-05-30  Include optimize module. Delete core module.
 2018-05-22  Include qdl module.
 2018-05-15  Include plot() as principal.
 2018-05-13  Include principal modules.
@@ -27,7 +28,6 @@ from __future__ import absolute_import, print_function, division
 
 import numpy as np                                        # noqa
 import pandas as pd                                       # noqa
-from fecon236.core import hellocore                       # noqa
 from fecon236.tool import *                               # noqa
 from fecon236.util import system                          # noqa
 from fecon236.host import fred                            # noqa
@@ -35,10 +35,11 @@ from fecon236.host import qdl                             # noqa
 from fecon236.visual import plots                         # noqa
 from fecon236.visual.plots import plot                    # noqa
 from fecon236.tsa import holtwinters as hw                # noqa
+from fecon236.oc import optimize as op                    # noqa
 
 
-__version__ = '10.6.2a29.post180522'
+__version__ = '10.6.3a35.post180530'
 #             ^HAND-CODE this string on MAJOR.MINOR releases.
-#              The alpha tag is based on Travis build number.
+#              The alpha or beta tag is based on Travis build number.
 #              The .post handle here is date-based.
 #  For current installed VERSION, use: "pip show fecon236"
