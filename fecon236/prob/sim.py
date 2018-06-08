0ac024361fe25e3b8fcac7fb73358b1fdc976a99
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-06-08 07:41:10 -0700

    prob/sim.py: Remove material on bootstrap

--- fecon236/prob/sim.py
+++ fecon236/prob/sim.py
@@ -1,18 +1,14 @@
-#  Python Module for import                           Date : 2018-06-07
+#  Python Module for import                           Date : 2018-06-08
 #  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
 '''
 _______________|  sim.py :: Simulation module for fecon236
 
 - Essential probabilistic functions for simulations.
-- Simulate Gaussian mixture model GM(2).
-- Normalize, include fat tails, so that mean and volatility can be specified.
-- Pre-compute pool of asset returns:
-    - SPX 1957-2014, requires fecon235/nb/SIMU-mn0-sd1pc-d4spx_1957-2014.csv.gz
-- Design bootstrap to study alternate histories and small-sample statistics.
-- Visualize price paths.
+- Handles Gaussian mixture model GM(2).
 
 
 CHANGE LOG  For LATEST version, see https://git.io/fecon236
+2018-06-08  Spin-off 2014 bootstrap material to boots/bootstrap.py.
 2018-06-07  Rename to sim.py, fecon236 fork. Pass flake8, fix imports.
 2017-06-29  yi_simulation.py, fecon235 v5.18.0312, https://git.io/fecon235
                 Let N generally be the count:= sample size.
@@ -22,16 +18,9 @@ from __future__ import absolute_import, print_function, division
 
 import numpy as np
 from fecon236.util import system
-from fecon236.tool import todf, georet
-from fecon236.host.fred import readfile
-from fecon236.visual.plots import plotn
 
-
-#  ACTUAL SPX mean and volatility from 1957-01-03 to 2014-12-11 in percent,
-#  where N = 15116
-MEAN_PC_SPX = 7.6306
-STD_PC_SPX = 15.5742
-N_PC_SPX = 15116
+#  from fecon236.visual.plots import plotn
+#  # TIP: Visualize price paths by using visual.plots.plotn()
 
 
 def randou(upper=1.0):
@@ -67,9 +56,11 @@ def simug(sigma, N=256):
     return arr
 
 
-def simug_mix(sigma1, sigma2, q=0.10, N=256):
+def simug_mix(sigma1=0.0969/16., sigma2=0.26/16., q=0.129, N=256):
     '''Simulate array from zero-mean Gaussian mixture GM(2).'''
-    #     Mathematical details in nb/gauss-mix-kurtosis.ipynb
+    #     Mathematical details in fecon235/nb/gauss-mix-kurtosis.ipynb
+    #     For SPX from 2007 to 2012, sigma1 was 9.69% annualized
+    #     sigma2 was 26% annualized, with q=0.1290, see https://git.io/gmix
     #  Pre-populate an array of shape (N,) with the FIRST Gaussian,
     #  so that most work is done quickly and memory efficient...
     arr = simug(sigma1, N)
@@ -81,86 +72,6 @@ def simug_mix(sigma1, sigma2, q=0.10, N=256):
         if maybe(q):
             arr[i] = randog(sigma2)
     return arr
-
-
-# =============================================================================
-
-
-def GET_simu_spx_pcent():
-    '''Retrieve normalized SPX daily percent change 1957-2014.'''
-    #           NORMALIZED s.t. sample mean=0 and std=1%.
-    datafile = 'SIMU-mn0-sd1pc-d4spx_1957-2014.csv.gz'
-    try:
-        df = readfile(datafile, compress='gzip')
-        #  print(' ::  Import success: ' + datafile)
-    except Exception:
-        df = 0
-        print(' !!  Failed to find: ' + datafile)
-    return df
-
-
-def SHAPE_simu_spx_pcent(mean=MEAN_PC_SPX, std=STD_PC_SPX):
-    '''Generate SPX percent change (defaults are ACTUAL annualized numbers).'''
-    #  Thus the default arguments can replicate actual time series
-    #  given initial value:  1957-01-02  46.20
-    #  Volatility is std:= standard deviation.
-    spxpc = GET_simu_spx_pcent()
-    mean_offset = mean / 256.0
-    #                    Assumed days in a year.
-    std_multiple = std / 16.0
-    #                    sqrt(256)
-    return (spxpc * std_multiple) + mean_offset
-
-
-def SHAPE_simu_spx_returns(mean=MEAN_PC_SPX, std=STD_PC_SPX):
-    '''Convert percent form to return form.'''
-    #  So e.g. 2% gain is converted to 1.02.
-    spxpc = SHAPE_simu_spx_pcent(mean, std)
-    return 1 + (spxpc / 100.0)
-
-
-def array_spx_returns(mean=MEAN_PC_SPX, std=STD_PC_SPX):
-    '''Array of SPX in return form.'''
-    #  Array far better than list because of numpy efficiency.
-    #        But if needed, use .tolist()
-    spxret = SHAPE_simu_spx_returns(mean, std)
-    #  Use array to conveniently bootstrap sample later.
-    #  The date index will no longer matter.
-    return spxret['Y'].values
-
-
-def bootstrap(N, yarray):
-    '''Randomly pick out N without replacment from yarray.'''
-    #  In repeated simulations, yarray should be pre-computed,
-    #                           using array_spx_returns(...).
-    #  http://docs.scipy.org/doc/numpy/reference/generated/numpy.random.choice.html
-    return np.random.choice(yarray, size=N, replace=False)
-
-
-def simu_prices(N, yarray):
-    '''Convert bootstrap returns to price time-series into pandas DataFrame.'''
-    #  Initial price implicitly starts at 1.
-    #  Realize that its history is just the products of the returns.
-    ret = bootstrap(N, yarray)
-    #               Cumulative product of array elements:
-    #               cumprod is very fast, and keeps interim results!
-    #  http://docs.scipy.org/doc/numpy/reference/generated/numpy.cumprod.html
-    return todf(np.cumprod(ret))
-
-
-def simu_plots_spx(charts=1, N=N_PC_SPX, mean=MEAN_PC_SPX, std=STD_PC_SPX):
-    '''Display simulated SPX price charts of N days, given mean and std.'''
-    yarray = array_spx_returns(mean, std)
-    #        Read in the data only once BEFORE the loop...
-    for i in range(charts):
-        px = simu_prices(N, yarray)
-        plotn(px)
-        #  Plot, then for the given prices, compute annualized:
-        #           geometric mean, arithmetic mean, volatility.
-        print('     georet: ' + str(georet(px)))
-        print('   ____________________________________')
-        print('')
-    return
 
 
 if __name__ == "__main__":
