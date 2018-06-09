8133cb1ef742921952984d36597b0f0f7881a86d
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-06-09 12:49:50 -0700

    prob/sim.py: Add rates2prices() and simshow()
    The latter provides a nice statistical and optional visual
    summary of the simulation module.

--- fecon236/prob/sim.py
+++ fecon236/prob/sim.py
@@ -1,26 +1,27 @@
-#  Python Module for import                           Date : 2018-06-08
+#  Python Module for import                           Date : 2018-06-09
 #  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
 '''
 _______________|  sim.py :: Simulation module for fecon236
 
 - Essential probabilistic functions for simulations.
 - Handles Gaussian mixture model GM(2).
-
+- Visualize simulated price paths, see simshow().
+- Let N be an integer for sample size or length of a series.
 
 CHANGE LOG  For LATEST version, see https://git.io/fecon236
+2018-06-09  Add rates2prices() and summary simshow().
 2018-06-08  Spin-off 2014 bootstrap material to boots/bootstrap.py.
 2018-06-07  Rename to sim.py, fecon236 fork. Pass flake8, fix imports.
 2017-06-29  yi_simulation.py, fecon235 v5.18.0312, https://git.io/fecon235
-                Let N generally be the count:= sample size.
 '''
 
 from __future__ import absolute_import, print_function, division
 
 import numpy as np
 from fecon236.util import system
-
-#  from fecon236.visual.plots import plotn
-#  # TIP: Visualize price paths by using visual.plots.plotn()
+from fecon236.tool import todf
+from fecon236.dst.gaussmix import gm2gem
+from fecon236.visual.plots import plotn
 
 
 def randou(upper=1.0):
@@ -48,30 +49,60 @@ def randog(sigma=1.0):
     return np.random.normal(loc=0.0, scale=sigma, size=None)
 
 
-def simug(sigma, N=256):
+def simug(sigma=0.13/16., N=256):
     '''Simulate array of shape (N,) from Gaussian Normal(0.0, sigma^2).'''
     #  Argument sigma is the standard deviation, NOT the variance!
-    arr = sigma * np.random.randn(N)
+    #  Default sigma is stylized per daily SPX data, see https://git.io/gmix
+    ratarr = sigma * np.random.randn(N)
     #  For non-zero mean, simply add it later: mu + simug(sigma)
-    return arr
+    return ratarr
 
 
 def simug_mix(sigma1=0.0969/16., sigma2=0.26/16., q=0.129, N=256):
     '''Simulate array from zero-mean Gaussian mixture GM(2).'''
-    #     Mathematical details in fecon235/nb/gauss-mix-kurtosis.ipynb
-    #     For SPX from 2007 to 2012, sigma1 was 9.69% annualized
-    #     sigma2 was 26% annualized, with q=0.1290, see https://git.io/gmix
-    #  Pre-populate an array of shape (N,) with the FIRST Gaussian,
-    #  so that most work is done quickly and memory efficient...
-    arr = simug(sigma1, N)
+    #  Default values are stylized per daily SPX data, see https://git.io/gmix
+    #  Mathematical details in fecon235/nb/gauss-mix-kurtosis.ipynb
+    #     Pre-populate an array of shape (N,) with the FIRST Gaussian,
+    #     so that most work is done quickly and memory efficient...
+    ratarr = simug(sigma1, N)
     #     ... except for some random replacements:
     for i in range(N):
         #                p = 1-q = probability drawing from FIRST Gaussian.
-        #  So with probability q, replace an element of arr
+        #  So with probability q, replace an element of ratarr
         #  with a float from the SECOND Gaussian:
         if maybe(q):
-            arr[i] = randog(sigma2)
-    return arr
+            ratarr[i] = randog(sigma2)
+    return ratarr
+
+
+def rates2prices(ratarr, mean=0/256., initialpx=0):
+    '''Convert array containing rates of return to DataFrame of prices.'''
+    #  Initial price, initialpx, if not set, implicitly starts at 1.
+    #  REMEMBER to adjust annualized mean to match data frequency.
+    ret = (1 + mean) + ratarr
+    #  Price history is just the products of the returns ret.
+    #      Cumulative product, cumprod(), of array elements is very fast.
+    #  http://docs.scipy.org/doc/numpy/reference/generated/numpy.cumprod.html
+    productret = np.cumprod(ret)
+    if initialpx:
+        prices = initialpx * productret
+    else:
+        prices = productret
+    return todf(prices)
+
+
+def simshow(repeat=3, mean=0/256., N=256, func=simug_mix, visual=True):
+    '''Statistical and optional visual SUMMARY: repeat simulations of func.'''
+    #  Function func should rely on all its default args, except for N.
+    for i in range(repeat):
+        istr = str(i)
+        ratarr = func(N=N)
+        prices = rates2prices(ratarr, mean, initialpx=100)
+        if visual:
+            plotn(prices, title='tmp-'+istr)
+        gm2gem(prices, b=2)
+        print('---------------------------------------' + istr)
+    return
 
 
 if __name__ == "__main__":
