5d9b22d7f1450ca961caa914da1800523f3d3e41
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-06-13 06:57:11 -0700

    ml/learn.py: Fix imports, pass flake8, pass doctest

--- fecon236/ml/learn.py
+++ fecon236/ml/learn.py
@@ -1,30 +1,31 @@
-#  Python Module for import                           Date : 2017-06-09
-#  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per Python PEP 0263 
-''' 
-_______________|  ys_mlearn.py : Machine learning tools
+#  Python Module for import                           Date : 2018-06-12
+#  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
+'''
+_______________|  learn.py :: Fundamentals from Machine Learning
 
 - softmax() for cross-entropy, MLE, neural networks, Boltzmann portfolio.
-  softmax_sort() for ordering and filtering info on the probabilities.
+- softmax_sort() for ordering and filtering info on the probabilities.
 
 
 REFERENCES
 - David J.C. MacKay (2008), Information theory, Inference, and Learning
-     Algorithms, 7th printing from Cambridge U. Press.
+    Algorithms, 7th printing from Cambridge U. Press.
 
-CHANGE LOG  For latest version, see https://github.com/rsvp/fecon235
-2017-06-09  Revise tau dependence on arrstable mean, not second max.
-2017-06-07  Add softmax_sort() with filter and renormalization.
-2017-06-06  First fecon235 version.
+
+CHANGE LOG  For LATEST version, see https://git.io/fecon236
+2018-06-12  learn.py, fecon236 fork. Fix imports, pass flake8, pass doctest.
+2017-06-09  ys_mlearn.py, fecon235 v5.18.0312, https://git.io/fecon235
 '''
 
 from __future__ import absolute_import, print_function, division
 
 import numpy as np
 from operator import itemgetter
-from . import yi_1tools as tools
+from fecon236 import tool
+from fecon236.util import system
 
 
-def softmax( it, temp=55, n=4 ):
+def softmax(it, temp=55, n=4):
     '''Softmax probabilities for iterable where temp sets temperature tau.
        Temperature tau is set as a temp percent of ensemble mean so that
        the scaling of tau works well across many different scenarios.
@@ -33,36 +34,36 @@ def softmax( it, temp=55, n=4 ):
        can be sharpened by decreasing temp towards 1.
        Setting temp to 0 results in generic softmax without temperature.
        Results are rounded to n decimal places.
-    >>> softmax( [ 16, 8, 4, 0, -8, -16 ], temp=200, n=4 )
+    >>> softmax([16, 8, 4, 0, -8, -16], temp=200, n=4)
     [0, 16, 0.2598, 200, [0.2598, 0.2001, 0.1757, 0.1542, 0.1188, 0.0915]]
-    >>> softmax( [ 16, 8, 4, 0, -8, -16 ], temp=50, n=4 )
+    >>> softmax([16, 8, 4, 0, -8, -16], temp=50, n=4)
     [0, 16, 0.5733, 50, [0.5733, 0.2019, 0.1198, 0.0711, 0.0251, 0.0088]]
-    >>> softmax( [ 16, 8, 4, 0, -8, -16 ], temp=30, n=4 )
+    >>> softmax([16, 8, 4, 0, -8, -16], temp=30, n=4)
     [0, 16, 0.7773, 30, [0.7773, 0.1365, 0.0572, 0.024, 0.0042, 0.0007]]
-    >>> softmax( [ 16, 8, 4, 0, -8, -16 ], temp=1, n=4 )
+    >>> softmax([16, 8, 4, 0, -8, -16], temp=1, n=4)
     [0, 16, 1.0, 1, [1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-    >>> softmax( [ 16, 8, 4, 0, -8, -16 ], temp=0, n=4 )
+    >>> softmax([16, 8, 4, 0, -8, -16], temp=0, n=4)
     [0, 16, 0.9997, 0, [0.9997, 0.0003, 0.0, 0.0, 0.0, 0.0]]
-    >>> softmax( [ 16, 16 ], temp=200, n=4 )
+    >>> softmax([16, 16], temp=200, n=4)
     [0, 16, 0.5, 200, [0.5, 0.5]]
-    >>> softmax( [ 16, 15, -8 ], temp=50, n=4 )
+    >>> softmax([16, 15, -8], temp=50, n=4)
     [0, 16, 0.5587, 50, [0.5587, 0.4395, 0.0018]]
     '''
     #  Reference: https://compute.quora.com/What-is-softmax
     #  Convert iterable to numpy array, then find index of its maximum value:
-    arr = tools.toar( it )
-    idmax = np.argmax( arr )
+    arr = tool.toar(it)
+    idmax = np.argmax(arr)
     hardmax = arr[idmax]
     #  Without loss of generality for mathematically defined softmax,
     #  subtracting an arbitrary constant from each it-element
     #  always helps NUMERICAL STABILITY, so let it be hardmax:
     arrstable = arr - hardmax
-        #  Important to note arrstable will consist of maximum(s) represented
-        #  by zero, and all other values will be necessarily negative.
+    #      Important to note arrstable will consist of maximum(s) represented
+    #      by zero, and all other values will be necessarily negative.
     if temp > 0:
         avg = np.mean(arrstable)
         if avg:
-            tau = abs( avg * (temp/100.0) )
+            tau = abs(avg * (temp/100.0))
             #  Let temperature be POSITIVE and temp percent of ensemble mean.
         else:
             #  Edge case: avg will be zero if it-scores are all equal,
@@ -75,14 +76,13 @@ def softmax( it, temp=55, n=4 ):
         temp = 0   # Prefer the numerical eqivalent for return below.
         tau = 1.0
     #  MATHEMATICALLY, (Boltzmann) softmax is defined as follows:
-    expit = np.exp( arrstable / tau )
-    sum_expit = np.sum( expit )
+    expit = np.exp(arrstable / tau)
+    sum_expit = np.sum(expit)
     softmax_exact = expit / sum_expit
     #                      roundit will output a list, not an array:
-    softmax_approx = tools.roundit( softmax_exact, n, echo=False )
+    softmax_approx = tool.roundit(softmax_exact, n, echo=False)
     hardprob = softmax_approx[idmax]
-    return [ idmax, hardmax, hardprob, temp, softmax_approx ]
-
+    return [idmax, hardmax, hardprob, temp, softmax_approx]
 
     #      __________ SOFTMAX USAGE NOTES
     #      softmax_sort() is obviously slower to compute than softmax().
@@ -98,7 +98,7 @@ def softmax( it, temp=55, n=4 ):
     #      To test temperature variations, softmax() will be much faster.
 
 
-def softmax_sort( it, temp=55, n=4, drop=0.00, renorm=False ):
+def softmax_sort(it, temp=55, n=4, drop=0.00, renorm=False):
     '''Softmax results sorted, include index; option to drop and renormalize.
        Probabilities less than drop are ignored.
        Setting renorm=True will make probabilities sum to 1.
@@ -107,17 +107,17 @@ def softmax_sort( it, temp=55, n=4, drop=0.00, renorm=False ):
     >>> softmax_sort([-16, -8, 0, 4, 8, 16], temp=50, drop=0.05, renorm=True)
     [(0.5934, 5, 16.0), (0.209, 4, 8.0), (0.124, 3, 4.0), (0.0736, 2, 0.0)]
     '''
-    arr = tools.toar( it )
+    arr = tool.toar(it)
     softmax_approx = softmax(arr, temp, n)[-1]
-    tups = [ (p, i, float(arr[i]))  for i, p in enumerate(softmax_approx) 
-                                        if p >= drop ]
-    #        ^so tuples are formatted as (probability, index, it-value).
+    #  Tuples are formatted as (probability, index, it-value)
+    tups = [(p, i, float(arr[i]))
+            for i, p in enumerate(softmax_approx) if p >= drop]
     if renorm:
         subtotal = sum([p for p, i, v in tups])
-        tups = [(round(p/subtotal, n), i, v) for p, i, v in tups] 
+        tups = [(round(p/subtotal, n), i, v) for p, i, v in tups]
     #  Want softmax_sort()[0] to yield the maximum candidate:
-    return sorted( tups, key=itemgetter(2), reverse=True )
+    return sorted(tups, key=itemgetter(2), reverse=True)
 
 
 if __name__ == "__main__":
-     system.endmodule()
+    system.endmodule()
