3f0825940e917651a9e48ed63e82a90306068cc2
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-06-15 09:02:05 -0700

    host/hostess.py: Faster get(). Pass flake8.
    Raised error messaging improved for getstock().

--- fecon236/host/hostess.py
+++ fecon236/host/hostess.py
@@ -1,9 +1,11 @@
-#  Python Module for import                           Date : 2018-06-14
+#  Python Module for import                           Date : 2018-06-15
 #  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
 '''
 _______________|  hostess.py :: Brings together fecon236 host modules
 
 CHANGE LOG  For LATEST version, see https://git.io/fecon236
+2018-06-15  Faster get() by exploiting startswith('s4'). Pass flake8.
+                Raised error messaging improved for getstock().
 2018-06-14  Spin-off get() from top.py.
 2018-03-12  fecon235.py, fecon235 v5.18.0312, https://git.io/fecon235
 '''
@@ -24,7 +26,7 @@ def get(code, maxi=0):
 
     get() will accept the vendor code directly as string, e.g.
     from FRED and Quandl, or use one of our abbreviated variables
-    documented in the appropriate module listed above.
+    documented in the appropriate module listed in the import section.
     The notebooks provide good code examples in action.
 
     Futures slang is of the form 'f4spotyym' where
@@ -37,22 +39,25 @@ def get(code, maxi=0):
     The general form is 's4symbol' where the symbol must be in
     lower case, so for SPY, use 's4spy' as an argument.
     '''
-    try:
-        df = getfred(code)
-    except:
+    if code.startswith('s4'):
+        #  Let failure report its real cause, e.g. RemoteDataError,
+        #  when disruption of equities data occurs;
+        #  see https://github.com/rsvp/fecon235/issues/7
+        if maxi:
+            df = getstock(code, maxi)
+        else:
+            df = getstock(code)
+    else:
         try:
-            if maxi:
-                df = getqdl(code, maxi)
-            else:
-                df = getqdl(code)
-        except:
+            df = getfred(code)
+        except Exception:
             try:
                 if maxi:
-                    df = getstock(code, maxi)
+                    df = getqdl(code, maxi)
                 else:
-                    df = getstock(code)
-            except:
-                raise ValueError('INVALID string or code for fecon get()')
+                    df = getqdl(code)
+            except Exception:
+                raise ValueError('INVALID symbol string or code variable.')
     return df
 
 
