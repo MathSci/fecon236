98c9ef8732ec8f2b1fb4cf44a4e9f9e227a714bd
Adriano rsvp.github.com <rsvp@users.noreply.github.com>
2018-06-15 21:33:35 -0700

    Add fecon236/util/group.py, spin-off from top.py
    Fix imports in the group module and explicit references to them.
    Obviously huge chunk of code removed from the top module.

--- fecon236/top.py
+++ fecon236/top.py
@@ -14,6 +14,8 @@ REFERENCE
   instruments is now accessible via OpenFIGI, https://www.openfigi.com/search
 
 CHANGE LOG  For LATEST version, see https://git.io/fecon236
+2018-06-14  Spin-off group stuff to util/group.py.
+                Spin-off get() to host/hostess.py.
 2018-06-13  Rename to top.py, fecon236 fork. Lint bugs:
                 too many undefined names (module specs) and bare excepts.
 2018-03-12  fecon235.py, fecon235 v5.18.0312, https://git.io/fecon235
@@ -23,27 +25,6 @@ from __future__ import absolute_import, print_function, division
 
 import pandas as pd
 from fecon236.util import system
-
-
-#  GROUPS:  specify our favorite series as a dictionary
-#  where key is name, and corresponding value is its data code:
-
-group4d = {'Zero10': d4zero10, 'SPX': d4spx, 'XAU': d4xau,
-           'EURUSD': d4eurusd, 'USDJPY': d4usdjpy}
-#         For usage, see fred-georeturns.ipynb for details,
-#         in particular, functions like group*() in this module.
-
-cotr4w = {'Bonds': w4cotr_bonds, 'Equities': w4cotr_equities,
-          'Metals': w4cotr_metals, 'USD': w4cotr_usd}
-#         For usage, see qdl-COTR-positions.ipynb for details,
-#         "Market position indicators using CFTC COTR"
-#         COTR:= Commitment of Traders Report.
-
-world4d = {'America': 's4spy', 'Europe': 's4ezu',
-           'Japan': 's4ewj',  'Emerging': 's4eem', 'Gold': 's4gld'}
-#         For usage, see prtf-boltzmann-1.ipynb for details,
-#         Exchange Traded Funds (ETF) of daily frequency
-#         representing EQUITIES worldwide plus gold.
 
 
 def get(code, maxi=0):
@@ -135,130 +116,6 @@ def holtfred(data, h=24, alpha=hw_alpha, beta=hw_beta):
     '''Holt-Winters forecast h-periods ahead (fredcode aware).'''
     #  Retained for backward compatibility, esp. pre-2016 notebooks.
     return foreholt(data, h, alpha, beta)
-
-
-def groupget(ggdic=group4d, maxi=0):
-    '''Retrieve and create group dataframe, given group dictionary.'''
-    #  Since dictionaries are unordered, create SORTED list of keys:
-    keys = [key for key in sorted(ggdic)]
-    #  Download individual dataframes as values into a dictionary:
-    dfdic = {key: get(ggdic[key], maxi) for key in keys}
-    #             ^Illustrates dictionary comprehension.
-    #  Paste together dataframes into one large sorted dataframe:
-    groupdf = paste([dfdic[key] for key in keys])
-    #  Name the columns:
-    groupdf.columns = keys
-    return groupdf
-
-
-def groupfun(fun, groupdf, *pargs, **kwargs):
-    '''Use fun(ction) column-wise, then output new group dataframe.'''
-    #  In math, this is known as an "operator":
-    #           a function which takes another function as argument.
-    #  Examples of fun: pcent, normalize, etc. See grouppc() next.
-    #  See groupget() to retrieve and create group dataframe.
-    keys = list(groupdf.columns)
-    #  Compute individual columns as dataframes in a list:
-    out = [todf(fun(todf(groupdf[key]), *pargs, **kwargs)) for key in keys]
-    #            ^Python 2 and 3 compatible: apply() removed in Python 3.
-    #  Paste together dataframes into one large dataframe:
-    outdf = paste(out)
-    #  Name the columns:
-    outdf.columns = keys
-    return outdf
-
-
-def grouppc(groupdf, freq=1):
-    '''Create overlapping pcent dataframe, given a group dataframe.'''
-    #  See groupget() to retrieve and create group dataframe.
-    #  Very useful to visualize as boxplot, see fred-georeturns.ipynb
-    return groupfun(pcent, groupdf, freq)
-
-
-def groupdiflog(groupdf, lags=1):
-    '''Difference between lagged log(data) for columns in group dataframe.'''
-    #  See groupget() to retrieve and create group dataframe.
-    return groupfun(diflog, groupdf, lags)
-
-
-def covdiflog(groupdf, lags=1):
-    '''Covariance array for differenced log(column) from group dataframe.
-       For correlation array: apply yi_matrix.cov2cor() later.
-    '''
-    #  See groupget() to retrieve and create group dataframe.
-    rates = groupdiflog(groupdf, lags)
-    V = rates.cov()
-    #        ^Type of V is still pandas DataFrame, so convert to array.
-    #  AVOID the np.matrix subclass; stick with np.ndarrays instead:
-    return V.values
-
-
-def groupgeoret(groupdf, yearly=256, order=True):
-    '''Geometric mean returns, non-overlapping, for group dataframe.
-       Argument "yearly" refers to annual frequency, e.g.
-       256 for daily trading days, 12 for monthly, 4 for quarterly.
-       ___ATTN___ Use groupgemrat() instead for greater accuracy.
-    '''
-    keys = list(groupdf.columns)
-    #  Use list comprehension to store lists from georet():
-    geo = [georet(todf(groupdf[k]), yearly) + [k] for k in keys]
-    #  where each georet list gets appended with an identifying key.
-    if order:
-        geo.sort(reverse=True)
-        #  Group is ordered in-place with respect to decreasing georet.
-    return geo
-
-
-def groupgemrat(groupdf, yearly=256, order=False, n=2):
-    '''Geometric mean rates, non-overlapping, for group dataframe.
-       Argument "yearly" refers to annual frequency, e.g.
-       256 for daily trading days, 12 for monthly, 4 for quarterly.
-       Output is rounded to n-decimal places.
-       Algorithm takes KURTOSIS into account for greater accuracy.
-    '''
-    keys = list(groupdf.columns)
-    #  Use list comprehension to store lists from gemrat():
-    gem = [roundit(gemrat(todf(groupdf[k]), yearly), n, echo=False)
-           + [k] for k in keys]
-    #      ^each gemrat list gets appended with an identifying key.
-    if order:
-        gem.sort(reverse=True)
-        #  Group is ordered in-place with respect to decreasing gemrat.
-    return gem
-
-
-def groupholtf(groupdf, h=12, alpha=ts.hw_alpha, beta=ts.hw_beta):
-    '''Holt-Winters forecasts h-periods ahead from group dataframe.'''
-    #  Tip: use all available (non-sliced) data for forecasting.
-    #  This is essentially a Kalman filter with optimal alpha-beta,
-    #  applied to each series individually, not jointly.
-    #  cf. holtfred() which works given a single series dataframe.
-    forecasts = []
-    keys = list(groupdf.columns)
-    for k in keys:
-        kdf = todf(groupdf[k])
-        holtdf = holt(kdf, alpha, beta)
-        forecastdf = holtforecast(holtdf, h)
-        forecasts.append(forecastdf)
-    keysdf = paste(forecasts)
-    keysdf.columns = keys
-    return keysdf
-
-
-def groupcotr(group=cotr4w, alpha=0):
-    '''Compute latest normalized CFTC COTR position indicators.
-       Optionally specify alpha for Exponential Moving Average
-       which is a smoothing parameter: 0 < alpha < 1 (try 0.26)
-       COTR is the Commitment of Traders Report from US gov agency.
-    '''
-    #  For detailed derivation, see qdl-COTR-positions.ipynb
-    positions = groupget(group)
-    norpositions = groupfun(normalize, positions)
-    #  alpha default should skip SMOOTHING operation...
-    if alpha:
-        return groupfun(ema, norpositions, alpha)
-    else:
-        return norpositions
 
 
 def forefunds(nearby='16m', distant='17m'):
---
+++ fecon236/util/group.py
@@ -0,0 +1,170 @@
+#  Python Module for import                           Date : 2018-06-14
+#  vim: set fileencoding=utf-8 ff=unix tw=78 ai syn=python : per PEP 0263
+'''
+_______________|  group.py :: Group utilities
+
+
+CHANGE LOG  For LATEST version, see https://git.io/fecon236
+2018-06-14  Spin-off group stuff from top.py.
+2018-03-12  fecon235.py, fecon235 v5.18.0312, https://git.io/fecon235
+'''
+
+from __future__ import absolute_import, print_function, division
+
+from fecon236 import tool
+from fecon236.util import system
+from fecon236.host import fred
+from fecon236.host import qdl
+from fecon236.host.hostess import get
+from fecon236.dst.gaussmix import gemrat
+from fecon236.tsa import holtwinters as hw
+
+
+#  GROUPS:  specify our favorite series as a dictionary
+#  where key is name, and corresponding value is its data code:
+
+group4d = {'Zero10': fred.d4zero10, 'SPX': fred.d4spx, 'XAU': fred.d4xau,
+           'EURUSD': fred.d4eurusd, 'USDJPY': fred.d4usdjpy}
+#         For usage, see https://git.io/georet for details,
+#         in particular, functions like group*() in this module.
+
+cotr4w = {'Bonds': qdl.w4cotr_bonds, 'Equities': qdl.w4cotr_equities,
+          'Metals': qdl.w4cotr_metals, 'USD': qdl.w4cotr_usd}
+#         For usage, see https://git.io/cotr for details,
+#         "Market position indicators using CFTC COTR"
+#         COTR:= Commitment of Traders Report.
+
+world4d = {'America': 's4spy', 'Europe': 's4ezu',
+           'Japan': 's4ewj',  'Emerging': 's4eem', 'Gold': 's4gld'}
+#         For usage, see https://git.io/boltz1 and https://git.io/boltz2
+#         for details, Exchange Traded Funds (ETF) of daily frequency
+#         representing EQUITIES worldwide plus gold.
+
+
+def groupget(ggdic=group4d, maxi=0):
+    '''Retrieve and create group dataframe, given group dictionary.'''
+    #  Since dictionaries are unordered, create SORTED list of keys:
+    keys = [key for key in sorted(ggdic)]
+    #  Download individual dataframes as values into a dictionary:
+    dfdic = {key: get(ggdic[key], maxi) for key in keys}
+    #           ^Illustrates dictionary comprehension.
+    #  Paste together dataframes into one large sorted dataframe:
+    groupdf = tool.paste([dfdic[key] for key in keys])
+    #  Name the columns:
+    groupdf.columns = keys
+    return groupdf
+
+
+def groupfun(fun, groupdf, *pargs, **kwargs):
+    '''Use fun(ction) column-wise, then output new group dataframe.'''
+    #  In mathematics, this is known as an "operator":
+    #      a function which takes another function as argument.
+    #  Examples of fun: pcent, normalize, etc. See grouppc() next.
+    #  See groupget() to retrieve and create group dataframe.
+    keys = list(groupdf.columns)
+    #  Compute individual columns as dataframes in a list:
+    out = [tool.todf(fun(tool.todf(groupdf[key]), *pargs, **kwargs))
+           for key in keys]
+    #   ^Python 2 and 3 compatible: apply() removed in Python 3.
+    #  Paste together dataframes into one large dataframe:
+    outdf = tool.paste(out)
+    #  Name the columns:
+    outdf.columns = keys
+    return outdf
+
+
+def grouppc(groupdf, freq=1):
+    '''Create overlapping pcent dataframe, given a group dataframe.'''
+    #  See groupget() to retrieve and create group dataframe.
+    #  Very useful to visualize as boxplot, see fred-georeturns.ipynb
+    return groupfun(tool.pcent, groupdf, freq)
+
+
+def groupdiflog(groupdf, lags=1):
+    '''Difference between lagged log(data) for columns in group dataframe.'''
+    #  See groupget() to retrieve and create group dataframe.
+    return groupfun(tool.diflog, groupdf, lags)
+
+
+#  def covdiflog(groupdf, lags=1):
+#      '''Covariance array for differenced log(column) from group dataframe.
+#         For correlation array: apply yi_matrix.cov2cor() later.
+#      '''
+#      #  See groupget() to retrieve and create group dataframe.
+#      rates = groupdiflog(groupdf, lags)
+#      V = rates.cov()
+#      #        ^Type of V is still pandas DataFrame, so convert to array.
+#      #  AVOID the np.matrix subclass; stick with np.ndarrays instead:
+#      return V.values
+
+
+def groupgeoret(groupdf, yearly=256, order=True):
+    '''Geometric mean returns, non-overlapping, for group dataframe.
+       Argument "yearly" refers to annual frequency, e.g.
+       256 for daily trading days, 12 for monthly, 4 for quarterly.
+       ATTN: Use groupgemrat() instead for greater accuracy.
+    '''
+    keys = list(groupdf.columns)
+    #  Use list comprehension to store lists from georet():
+    geo = [tool.georet(tool.todf(groupdf[k]), yearly) + [k] for k in keys]
+    #  where each georet list gets appended with an identifying key.
+    if order:
+        geo.sort(reverse=True)
+        #  Group is ordered in-place with respect to decreasing georet.
+    return geo
+
+
+def groupgemrat(groupdf, yearly=256, order=False, n=2):
+    '''Geometric mean rates, non-overlapping, for group dataframe.
+       Argument "yearly" refers to annual frequency, e.g.
+       256 for daily trading days, 12 for monthly, 4 for quarterly.
+       Output is rounded to n-decimal places.
+       Algorithm takes KURTOSIS into account for greater accuracy.
+    '''
+    keys = list(groupdf.columns)
+    #  Use list comprehension to store lists from gemrat():
+    gem = [tool.roundit(gemrat(tool.todf(groupdf[k]), yearly), n, echo=False)
+           + [k] for k in keys]
+    #      ^each gemrat list gets appended with an identifying key.
+    if order:
+        gem.sort(reverse=True)
+        #  Group is ordered in-place with respect to decreasing gemrat.
+    return gem
+
+
+def groupholtf(groupdf, h=12, alpha=hw.hw_alpha, beta=hw.hw_beta):
+    '''Holt-Winters forecasts h-periods ahead from group dataframe.'''
+    #  Tip: use all available (non-sliced) data for forecasting.
+    #  This is essentially a Kalman filter with optimal alpha-beta,
+    #  applied to each series individually, not jointly.
+    #  cf. holtfred() which works given a single series dataframe.
+    forecasts = []
+    keys = list(groupdf.columns)
+    for k in keys:
+        kdf = tool.todf(groupdf[k])
+        holtdf = hw.holt(kdf, alpha, beta)
+        forecastdf = hw.holtforecast(holtdf, h)
+        forecasts.append(forecastdf)
+    keysdf = tool.paste(forecasts)
+    keysdf.columns = keys
+    return keysdf
+
+
+def groupcotr(group=cotr4w, alpha=0):
+    '''Compute latest normalized CFTC COTR position indicators.
+       Optionally specify alpha for Exponential Moving Average
+       which is a smoothing parameter: 0 < alpha < 1 (try 0.26)
+       COTR is the Commitment of Traders Report from US gov agency.
+    '''
+    #  For detailed derivation, see qdl-COTR-positions.ipynb
+    positions = groupget(group)
+    norpositions = groupfun(tool.normalize, positions)
+    #  alpha default should skip SMOOTHING operation...
+    if alpha:
+        return groupfun(hw.ema, norpositions, alpha)
+    else:
+        return norpositions
+
+
+if __name__ == "__main__":
+    system.endmodule()
